f1aee4c0761f45d6dcdee1119edc1421a03aaa66
acze <kontakt@kucharski-webdeveloper.pl>
2016-12-16 14:08:29 +0100

    Add login/register, add custom field to register "gender"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Social Gamer App</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
 
         <!-- Styles -->
         <style>
@@ -79,16 +80,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Social Gamer App
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <p>Portal społecznościowy dla graczy.</p>
             </div>
         </div>
     </body>
