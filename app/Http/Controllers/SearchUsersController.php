affb8f9f4ebd4050432c0bf159cadc897073e193
acze <kontakt@kucharski-webdeveloper.pl>
2016-12-19 22:39:10 +0100

    Create Search users blade; Create Search Users Controller;

--- app/Http/Controllers/SearchUsersController.php
+++ app/Http/Controllers/SearchUsersController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use App\User;
 
 class SearchUsersController extends Controller
 {
     
     public function users()
     {
-        
+        $search_phrase = Input::get('s');
+        $results_search = User::where('name', 'like', '%' . $search_phrase . '%')->paginate(8);
+        return view('search.users', compact('results_search'));
     }
 }
---
+++ resources/views/search/users.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Wynik wyszukiwania
+                    </div>
+                    <div class="panel-body">
+                        @if ($results_search->count() === 0)
+                            <h4 class="text-center">Brak wyników wyszukiwania.</h4>
+                        @else
+                            <div class="row">
+                                @foreach ($results_search as $user)
+                                    <a href="{{ url('/users/' . $user->id) }}">
+                                        <div class="col-md-3">
+                                            <img src="{{ url('user-avatar/' . $user->id . '/300') }}" alt="" class="img-responsive">
+                                            <h5 class="text-center">{{ $user->name }}</h5>
+                                        </div>
+                                    </a>
+                                @endforeach
+                            </div>
+                            <div class="text-center">
+                                {{ $results_search }}
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
