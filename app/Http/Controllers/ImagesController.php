622006620034ec469a191a5b23150010d1c5986b
acze <kontakt@kucharski-webdeveloper.pl>
2016-12-30 23:16:18 +0100

    change image quality

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -14,14 +14,14 @@ class ImagesController extends Controller
         $user = User::findOrFail($id);
         
         if (strpos($user->avatar, 'http') !== false) {
-            $img = Image::make($user->avatar)->fit($size)->response('jpg', 100);
+            $img = Image::make($user->avatar)->fit($size)->response('jpg', 80);
         } elseif (is_null($user->avatar) && $user->gender == 'm') {
-            $img = Image::make(asset('images/default-avatar/man.png'))->fit($size)->response('png', 100);
+            $img = Image::make(asset('images/default-avatar/man.png'))->fit($size)->response('png', 80);
         } elseif (is_null($user->avatar) && $user->gender == 'f') {
-            $img = Image::make(asset('images/default-avatar/famale.png'))->fit($size)->response('png', 100);
+            $img = Image::make(asset('images/default-avatar/famale.png'))->fit($size)->response('png', 80);
         } else {
             $avatar_path = asset('storage/users/' . $id . '/avatars/' . $user->avatar);
-            $img = Image::make($avatar_path)->fit($size)->response('jpg', 100);
+            $img = Image::make($avatar_path)->fit($size)->response('jpg', 80);
         }
         
         return $img;
