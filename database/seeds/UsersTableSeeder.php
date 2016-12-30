7847866ccd8fdc967dbee046faad8ff91bb177b1
acze <kontakt@kucharski-webdeveloper.pl>
2016-12-30 02:40:15 +0100

    Create Friends seeder.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Friend;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 
@@ -15,14 +16,15 @@ class UsersTableSeeder extends Seeder
     {
         $faker = Faker::create('pl_PL');
 
-//      VARIABLES
+        //---// VARIABLES //---//
 
-        $user_numer = 18;
+        $user_number = 19;
         $password = '1';
 
-        for ($i = 1; $i <= $user_numer; $i ++) {
+        //---// USERS //---//
+        for ($user_id = 1; $user_id <= $user_number; $user_id ++) {
 
-            if ($i === 1) {
+            if ($user_id === 1) {
                 DB::table('users')->insert([
                     'name'     => 'Rafał Kucharski',
                     'email'    => 'kuchar.rafal@gmail.com',
@@ -38,31 +40,57 @@ class UsersTableSeeder extends Seeder
                     'avatar'   => '36b2f5ca723acb00d65f186ef8d93c55.jpeg',
                     'password' => bcrypt($password),
                 ]);
+            } else {
+
+                $gender = $faker->randomElement(['m', 'f']);
+
+                switch ($gender) {
+                    case 'm':
+                        $name = $faker->firstNameMale . ' ' . $faker->lastNameMale;
+                        $avatar = json_decode(file_get_contents('https://randomuser.me/api/?gender=male'))->results[0]->picture->large;
+                        break;
+
+                    case 'f':
+                        $name = $faker->firstNameFemale . ' ' . $faker->lastNameFemale;
+                        $avatar = json_decode(file_get_contents('https://randomuser.me/api/?gender=female'))->results[0]->picture->large;
+                        break;
+                }
+
+                DB::table('users')->insert([
+                    'name'     => $name,
+                    'email'    => str_replace('-', '.', str_slug($name)) . '@' . $faker->safeEmailDomain,
+                    'gender'   => $gender,
+                    'avatar'   => $avatar,
+                    'password' => bcrypt($password),
+                ]);
+
             }
 
-            $gender = $faker->randomElement(['m', 'f']);
+            //---// FRIENDS //---//
+            for ($i = 1; $i <= $faker->numberBetween($min = 0, $max = $user_number - 1); $i ++) {
+                $friend_id = $faker->numberBetween($min = 1, $max = $user_number);
 
-            switch ($gender) {
-                case 'm':
-                    $name = $faker->firstNameMale . ' ' . $faker->lastNameMale;
-                    $avatar = json_decode(file_get_contents('https://randomuser.me/api/?gender=male'))->results[0]->picture->large;
-                    break;
+                $friend_condition = Friend::where([
+                    'user_id'   => $user_id,
+                    'friend_id' => $friend_id,
+                ])->orWhere([
+                    'user_id'   => $friend_id,
+                    'friend_id' => $user_id,
+                ])->exists();
 
-                case 'f':
-                    $name = $faker->firstNameFemale . ' ' . $faker->lastNameFemale;
-                    $avatar = json_decode(file_get_contents('https://randomuser.me/api/?gender=female'))->results[0]->picture->large;
-                    break;
+                if (!$friend_condition) {
+
+                    DB::table('friends')->insert([
+                        'user_id'   => $user_id,
+                        'friend_id' => $friend_id,
+                        'accepted'    => 1,
+                        'created_at' => $faker->dateTimeThisYear($max = 'now')
+                    ]);
+
+                }
+
             }
-
-            DB::table('users')->insert([
-                'name'     => $name,
-                'email'    => str_replace('-', '.', str_slug($name)) . '@' . $faker->safeEmailDomain,
-                'gender'   => $gender,
-                'avatar'   => $avatar,
-                'password' => bcrypt($password),
-            ]);
 
         }
-
     }
 }
