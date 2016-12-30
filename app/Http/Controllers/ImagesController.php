52a438ae138d362b5d830d0f782558a9cfa6998c
acze <kontakt@kucharski-webdeveloper.pl>
2016-12-30 23:05:01 +0100

    Change default avatars

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -15,8 +15,10 @@ class ImagesController extends Controller
         
         if (strpos($user->avatar, 'http') !== false) {
             $img = Image::make($user->avatar)->fit($size)->response('jpg', 100);
-        } elseif (is_null($user->avatar)) {
-            $img = Image::make(asset('images/default-avatar.jpg'))->fit($size)->response('jpg', 100);
+        } elseif (is_null($user->avatar) && $user->gender == 'm') {
+            $img = Image::make(asset('images/default-avatar/man.png'))->fit($size)->response('png', 100);
+        } elseif (is_null($user->avatar) && $user->gender == 'f') {
+            $img = Image::make(asset('images/default-avatar/famale.png'))->fit($size)->response('png', 100);
         } else {
             $avatar_path = asset('storage/users/' . $id . '/avatars/' . $user->avatar);
             $img = Image::make($avatar_path)->fit($size)->response('jpg', 100);
