e72552b78bca08078d90e0cf926e7476465b9c05
acze <kontakt@kucharski-webdeveloper.pl>
2017-01-03 14:16:52 +0100

    no message

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,7 +1,7 @@
 
 <div class="clearfix">
     <img src="{{ url('user-avatar/' . $comment->user->id . '/36') }}" alt="" class="img-responsive pull-left">
-    <div class="col-sm-11">
+    <div class="col-xs-10 col-sm-11">
         <div class="post-info small pull-left" >
             <p style="margin-bottom: 0px;"><strong><a href="{{ url('/users/' . $comment->user->id) }}">{{ $comment->user->name }}</a></strong> {{ $comment->body }}</p>
             <p><span style="margin-right: 5px" class="fa fa-clock-o"></span><a href="">{{ $comment->created_at->diffForHumans() }}</a></p>
