c7da29bdf79b08314500469ee519a143f6de6d85
acze <kontakt@kucharski-webdeveloper.pl>
2017-01-03 14:15:22 +0100

    minor fixes

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,8 +1,11 @@
 
 <div class="clearfix">
     <img src="{{ url('user-avatar/' . $comment->user->id . '/36') }}" alt="" class="img-responsive pull-left">
-    <div style="margin-left: 10px;" class="post-info small pull-left">
-        <p style="margin-bottom: 0px;"><strong><a href="{{ url('/users/' . $comment->user->id) }}">{{ $comment->user->name }}</a></strong> {{ $comment->body }}</p>
-        <p><span style="margin-right: 5px" class="fa fa-clock-o"></span><a href="">{{ $comment->created_at->diffForHumans() }}</a></p>
+    <div class="col-sm-11">
+        <div class="post-info small pull-left" >
+            <p style="margin-bottom: 0px;"><strong><a href="{{ url('/users/' . $comment->user->id) }}">{{ $comment->user->name }}</a></strong> {{ $comment->body }}</p>
+            <p><span style="margin-right: 5px" class="fa fa-clock-o"></span><a href="">{{ $comment->created_at->diffForHumans() }}</a></p>
+        </div>
     </div>
+
 </div>
