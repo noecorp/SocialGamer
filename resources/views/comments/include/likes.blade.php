002895022333ebb1d6a67f6f34e1000cfb53a79d
acze <kontakt@kucharski-webdeveloper.pl>
2017-02-18 17:15:46 +0100

    no message

--- resources/views/comments/include/likes.blade.php
+++ resources/views/comments/include/likes.blade.php
@@ -1,14 +1,22 @@
-@if(isLiked('comment_id', $comment->id))
-    <form method="POST" action="{{ url('/likes/') }}" class="">
-        {{ csrf_field() }}
-        {{ method_field('DELETE') }}
-        <input type="hidden" name="comment_id" value="{{ $comment->id }}">
-        <button class="btn btn-primary btn-xs pull-left" type="submit"><i class="fa fa-thumbs-up"></i> {{ $comment->likes()->count() }}</button>
-    </form>
+@if(Auth::check())
+
+    @if(isLiked('comment_id', $comment->id))
+        <form method="POST" action="{{ url('/likes/') }}" class="">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <input type="hidden" name="comment_id" value="{{ $comment->id }}">
+            <button class="btn btn-primary btn-xs pull-left" type="submit"><i class="fa fa-thumbs-up"></i> {{ $comment->likes()->count() }}</button>
+        </form>
+    @else
+        <form method="POST" action="{{ url('/likes') }}" class="">
+            {{ csrf_field() }}
+            <input type="hidden" name="comment_id" value="{{ $comment->id }}">
+            <button class="btn btn-default btn-xs pull-left" type="submit"><i class="fa fa-thumbs-up"></i> {{ $comment->likes()->count() }}</button>
+        </form>
+    @endif
+
 @else
-    <form method="POST" action="{{ url('/likes') }}" class="">
-        {{ csrf_field() }}
-        <input type="hidden" name="comment_id" value="{{ $comment->id }}">
-        <button class="btn btn-default btn-xs pull-left" type="submit"><i class="fa fa-thumbs-up"></i> {{ $comment->likes()->count() }}</button>
-    </form>
+
+    <button class="btn btn-default btn-xs pull-left" type="submit"><i class="fa fa-thumbs-up"></i> {{ $comment->likes()->count() }}</button>
+
 @endif
